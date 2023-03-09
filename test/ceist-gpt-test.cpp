d0a2a075f127d8a9601f6e4550c6adf2afd85717
Shane Hamilton <shane.hamilton3@mail.dcu.ie>
2023-03-09 21:10:10 +0000

    Update ceist-gpt-test.cpp

--- test/ceist-gpt-test.cpp
+++ test/ceist-gpt-test.cpp
@@ -1,11 +1,12 @@
-#include <gtest/gtest.h>
-#include <ceist-gpt/ceist-gpt.hpp>
+#include <ceist-gpt.hpp>
+#include <catch2/catch.hpp>
 
-TEST(CeistGptTest, ExampleTest) {
-    // Create a CeistGpt instance and call the generate_text method.
-    ceist_gpt::CeistGpt ceist_gpt;
-    std::string response = ceist_gpt.generate_text("Hello, world!");
-
-    // Check that the response is not empty.
-    ASSERT_FALSE(response.empty());
+TEST_CASE("Test CeistGpt API query", "[ceist-gpt]") {
+    ceist::CeistGpt ceist_gpt;
+    std::string prompt = "Hello,";
+    std::string response = ceist_gpt.query(prompt);
+    REQUIRE(!response.empty());
 }
+
+// Add more test cases here...
+
